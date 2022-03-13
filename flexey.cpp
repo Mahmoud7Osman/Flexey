6bb3f3da263f93a9b274d73a6336ba2ea3aeffd0
Mahmoud Osman <60033959+Mahmoud7Osman@users.noreply.github.com>
2022-03-13 22:36:16 +0200

    Add files via upload

--- flexey.cpp
+++ flexey.cpp
@@ -7,13 +7,15 @@
 using std::string;
 
 typedef struct node{
-	char* data;
+	char data[16];
 	struct node* next;
 }list;
 
 string input;
-
 bool Exit=false;
+
+node	first_node, *head=&first_node, *tmp=&first_node, *tail=&first_node;
+
 
 const char* menu="\n\n\n\n\nFlexey, This Program Will Demonstrate To You How Linked Lists Works, Created For C And C++ Students (Data Structures And Algorithms Beginners)\n\n\n"
 		"	0> Exit\n"
@@ -22,18 +24,23 @@ const char* menu="\n\n\n\n\nFlexey, This Program Will Demonstrate To You How Lin
 		"	3> Insert New Node\n"
 		"	4> Delete A Node\n"
 		"	5> Edit A Specific Node\n"
-		"	6> Change A Specific Node Location\n"
-		"	7> Change A Specific Node Connection\n"
-		"	8> Cut A Specific Node Connection\n"
-		"	9> Save The Linked List in a File\n\n\n";
+		"	6> Change A Specific Node Connection\n"
+		"	7> Cut A Specific Node Connection\n"
+		"	8> Save The Linked List in a File\n\n\n";
 
 
 
+void append();
+void insert(int);
 void reader();
+void read(int);
 void freelist();
+void delete(int);
 
 int main(){
 	printf("%s", menu);
+
+	strcpy (first_node.data, "Flexey");
 
 	while(!Exit){
 		reader();
@@ -49,7 +56,7 @@ void reader(){
 
 	if (!strcmp(input.c_str(), "0"))
 		freelist();
-	
+
 }
 void freelist(){
 	return;
